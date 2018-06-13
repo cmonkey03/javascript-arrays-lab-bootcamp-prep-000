a0dd3985b3611b03046f7209ffcc42e3bbd5edbf
russellpbrown@gmail.com <russellpbrown@gmail.com>
2018-06-13 11:24:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var moreKittens = kittens.push(name)
+  moreKittens = kittens.push(name)
   return moreKittens
 }
 
